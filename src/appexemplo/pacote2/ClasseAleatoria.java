5023af90d9eb0278a4a4ac548d3c29c06daf728e
31319769 <31319769@099503-P33S102.macklabs.br>
2014-09-02 08:35:37 -0300

    Commit Teste

---
+++ src/appexemplo/pacote2/ClasseAleatoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appexemplo.pacote2;
+
+/**
+ *
+ * @author 31319769
+ */
+public class ClasseAleatoria {
+    
+    public static void main(String[] args) {
+        while(true)
+            System.out.println("Teste");
+    }
+    
+}
