dff58da4daad1615cf287cc1d31d807788db9dc7
31300022 <31300022@099510-P33S102.macklabs.br>
2014-09-02 08:48:36 -0300

    Nao faz nada

---
+++ src/appexemplo/pacote2/ClasseDoJuvi.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appexemplo.pacote2;
+
+/**
+ *
+ * @author 31300022
+ */
+public class ClasseDoJuvi {
+    
+}
