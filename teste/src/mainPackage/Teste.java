ebf9d931e302051a07214c2e28a30ffef5d82181
maxinho <maxinho@Rohling>
2014-09-06 15:50:05 -0300

    Commit inicial do projeto

---
+++ teste/src/mainPackage/Teste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainPackage;
+
+/**
+ *
+ * @author maxinho
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Good night moon!");
+        System.out.println(":D");
+    }
+}
