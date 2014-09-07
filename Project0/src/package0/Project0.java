13e1ce6e543050136116dde43a7e37e00198ae95
Bruom <Bruom@192.168.0.108>
2014-09-07 19:08:15 -0300

    Commit teste - tomara que não quebre nada...

---
+++ Project0/src/package0/Project0.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package package0;
+
+/**
+ *
+ * @author Bruom
+ */
+public class Project0 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("All that which is great, once started small.");
+        System.out.println("All that which is many, was once one.");
+        System.out.println("And all that ever was one, truly began on zero.");
+        
+    }
+    
+}
